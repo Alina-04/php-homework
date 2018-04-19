c620009ce3ee3bd3cd2586f753c3b22ac35f6387
Alina-04 <alina.lu04@gmail.com>
2018-04-19 16:26:02 +0300

    1.php

---
+++ 1-30.03.18/1.php
@@ -0,0 +1,19 @@
+<?php
+	
+	// Объявите переменные типов: Число, число с плавающей точкой, строка, булево значение, нул, массив.
+
+
+	$chislo = 1;
+	$chisloSTochkoi = 2.3;
+	$stroka = 'privet';
+	$bulevo = true;
+	$nul = NULL;
+	$massiv = [1,2,3];
+		var_dump($chislo);
+		var_dump($chisloSTochkoi);
+		var_dump($stroka);
+		var_dump($bulevo);
+		var_dump($nul);
+		var_dump($massiv);
+	
+?>
